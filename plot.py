03c3fcf1c7402e89c13971c14596b29ab0ce906e
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-09-11 01:08:28 -0300

    update

--- plot.py
+++ plot.py
@@ -1,14 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
 
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from collections import namedtuple
 
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
-import matplotlib.pyplot as plt
 
 batch_size = 32
 num_classes = 10
@@ -23,22 +19,32 @@ accuracy = [0.4985, 0.5836, 0.5973, 0.6044, 0.6090, 0.6212, 0.6200, 0.6150, 0.62
 
 loss = [4.1595, 1.5810, 1.6032, 1.5167, 1.5673, 1.6020, 1.5877, 1.4854, 1.4932, 1.5265]
     
-# grafico 1
-plt.plot(accuracy, 'o--')
-plt.title("Worst config accuracy")
-plt.ylabel("Accuracy")
-plt.xlabel("epoch")
-plt.show()
+# # grafico 1
+# plt.plot(accuracy, 'o--')
+# plt.title("Worst config accuracy")
+# plt.ylabel("Accuracy")
+# plt.xlabel("epoch")
+# plt.show()
 
 
-# grafico 2
-objects = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-y_pos = np.arange(len(objects))
-performance = accuracy
+# # grafico 2
+# objects = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+# y_pos = np.arange(len(objects))
+# performance = accuracy
  
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Accuracy')
-plt.xlabel("epoch")
+# plt.bar(y_pos, performance, align='center', alpha=0.5)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Accuracy')
+# plt.xlabel("epoch")
+# plt.title('Worst config accuracy') 
+# plt.show()
+
+
+
+x = accuracy
+y = loss
+plt.ylabel("loss")
+plt.xlabel("accuracy")
 plt.title('Worst config accuracy') 
+plt.scatter(x, y)
 plt.show()
